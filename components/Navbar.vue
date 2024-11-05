24371c0e1fddf7a70b7560ee38f216d8d5fa594a
Ines Alvergne <i.alvergne@gmail.com>
2024-11-05 18:44:04 +0100

    burger icon

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,29 +1,52 @@
-<script setup></script>
-
 <template>
-  <!-- On Desktop -->
-  <div class="border border-brick rounded-xl w-fit mx-auto mobile: hidden tablet:hidden">
-    <div class="flex gap-10 py-4 px-10 text-brick">
-      <NuxtLink to="/about" class="hover:text-gradient">
-        ABOUT ME
-      </NuxtLink>
+  <div>
+    <!-- On Desktop -->
+    <div class="hidden tablet:block border border-brick rounded-xl w-fit mx-auto">
+      <div class="flex gap-10 py-4 px-10 text-brick">
+        <NuxtLink to="/about" class="hover:text-gradient">
+          ABOUT ME
+        </NuxtLink>
 
-      <NuxtLink to="/services" class="hover:text-gradient ">
-        SERVICES
-      </NuxtLink>
+        <NuxtLink to="/services" class="hover:text-gradient ">
+          SERVICES
+        </NuxtLink>
 
-      <NuxtLink to="/projects" class="hover:text-gradient ">
-        PROJECTS
-      </NuxtLink>
+        <NuxtLink to="/projects" class="hover:text-gradient ">
+          PROJECTS
+        </NuxtLink>
 
-      <NuxtLink to="/get-in-touch" class="hover:text-gradient ">
-        GET IN TOUCH
-      </NuxtLink>
+        <NuxtLink to="/get-in-touch" class="hover:text-gradient ">
+          GET IN TOUCH
+        </NuxtLink>
+      </div>
+    </div>
+
+    <!-- on Mobile -->
+    <div class="laptop:hidden flex justify-end p-6 bg-white">
+      <IconsBurger />
+    </div>
+    <div class="laptop:hidden z-[1] absolute top-[70px] inset-x-0 h-fit w-full text-right py-5 px-7 bg-white shadow-md">
+      <div class="flex flex-col align-items-end gap-6 mt-3 text-brick">
+        <NuxtLink to="/about" class="hover:text-gradient">
+          ABOUT ME
+        </NuxtLink>
+
+        <NuxtLink to="/services" class="hover:text-gradient ">
+          SERVICES
+        </NuxtLink>
+
+        <NuxtLink to="/projects" class="hover:text-gradient ">
+          PROJECTS
+        </NuxtLink>
+
+        <NuxtLink to="/get-in-touch" class="hover:text-gradient ">
+          GET IN TOUCH
+        </NuxtLink>
+      </div>
     </div>
   </div>
-
-  <!-- on Mobile -->
-  <div class="w-screen flex justify-between items-center px-10 py-4 laptop:hidden">
-    <p>salut</p>
-  </div>
 </template>
+
+<script setup>
+
+</script>
