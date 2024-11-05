163733735faf5a031618817d4b96d528e7e10529
Ines Alvergne <i.alvergne@gmail.com>
2024-11-05 18:56:55 +0100

    transitions

--- components/FadeSlideTransition.vue
+++ components/FadeSlideTransition.vue
@@ -2,7 +2,18 @@
   <Transition name="fadeslide" appear>
     <slot></slot>
   </Transition>
+
 </template>
+
+<script>
+  // to use transition on list items (with v-for) --> use TransitionGroup
+  // and we can add a tag on it ex:
+  // <TransitionGroup name="fadeslide" tag="ul">
+  //   <li v-for="item in items" :key="item.id">
+  //     {{ item.name }}
+  //   </li>
+  // </TransitionGroup>
+</script>
 
 <style>
   .fadeslide-enter-active, .fadeslide-leave-active {
