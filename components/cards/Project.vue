750c4cd7603397ebc6498f535cf013ebe45b19c4
Ines Alvergne <i.alvergne@gmail.com>
2024-12-03 17:37:26 +0100

    wip

---
+++ components/cards/Project.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex">
+    <div class="w-2/5 bg-transparent-pastel-1 rounded-l-lg p-10">
+      <img :src="image" alt="mobile mockup of karmalama" width="350" class="m-auto">
+    </div>
+
+    <div class="w-3/5 bg-white rounded-r-lg p-12 flex flex-col justify-center">
+      <h2 class="mb-5">{{ title }}</h2>
+      <p class="mb-5">{{ description }}</p>
+      <div class="flex gap-5 text-[14px]">
+        <div class="border border-brick text-brick rounded-sm p-2">
+          Full-Stack
+        </div>
+        <div class="border border-brick text-brick rounded-sm p-2">
+          UI design
+        </div>
+        <div class="border border-brick text-brick rounded-sm p-2">
+          Email design & development
+        </div>
+        <div class="border border-brick text-brick rounded-sm p-2">
+          Admin dashboard
+        </div>
+      </div>
+      <div class="flex justify-between border-t border-t-brick mt-10 pt-5">
+        <p><span class="font-bold">Role:</span> {{ role }}</p>
+        <p>View details →</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    props: {
+      title: String,
+      description: String,
+      role: String,
+      image: String
+    }
+  }
+</script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,43 @@
 <template>
-  <div class="page-head">
-    <h1>Projects</h1>
-    <p>Bacon ipsum dolor amet pork belly alcatra boudin meatloaf, cow doner beef ribs prosciutto pancetta tri-tip picanha.</p>
+  <div>
+    <div class="page-head">
+      <h1>Discover a few projects I've worked on</h1>
+      <p>Bacon ipsum dolor amet pork belly alcatra boudin meatloaf, cow doner beef ribs prosciutto pancetta tri-tip picanha.</p>
+    </div>
+
+    <div class="w-4/5 mx-auto flex flex-col gap-12">
+      <CardsProject
+        v-for="(project, index) in projects"
+        :key="index"
+        :title="project.title"
+        :description="project.description"
+        :role="project.role"
+        :image="project.image"
+      />
+    </div>
   </div>
 </template>
+
+<script setup>
+  import karmaLamaImage from '@/assets/images/mockups/karmalama.png';
+  import wallyImage from '@/assets/images/mockups/wally.png';
+  // import kYogaImage from '@/assets/images/mockups/kyoga.png';
+
+  const projects = [
+    { title: "Karma Lama",
+      description: "A web and mobile application to connect non-profit organisations with volunteers. Organisations can create and manage volunteer missions and volunteers can earn points for completing missions.",
+      role: "Rails Full-Stack Developer",
+      image: karmaLamaImage
+    },
+    { title: "Wally",
+      description: "A fractional investment platform to allow individuals and partners to invest in real-estate with no hassle.",
+      role: "Rails Full-Stack Developer",
+      image: wallyImage
+    },
+    { title: "K Yoga",
+      description: "A platform for yoga enthusiasts to practice everyday with live courses and explore different types of yoga with an extensive video library.",
+      role: "Lead Developer",
+      image: "@/assets/images/mockups/karmalama.png"
+    },
+  ]
+</script>
