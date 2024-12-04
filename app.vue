cc69ff92bc614473c8900ca9f8bfb6f177403ec7
Ines Alvergne <i.alvergne@gmail.com>
2024-12-04 23:28:39 +0100

    wip

--- app.vue
+++ app.vue
@@ -4,5 +4,6 @@
     <div class="w-11/12 laptop:w-9/12 desktop:w-7/12 mx-auto">
       <NuxtPage />
     </div>
+    <Footer />
   </NuxtLayout>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="bg-light-pink rounded-xl mt-32">
+    <div class="w-11/12 laptop:w-9/12 desktop:w-7/12 mx-auto">
+      <div class="h-[200px] py-10">
+        <div class="flex justify-between items-start">
+          <div>
+            <img src="@/assets/images/logo_full.png" alt="logo ines alvergne" width="100">
+            <p class="mt-5 text-pink">2024 Ines Alvergne EI </p>
+          </div>
+          <div>
+            <div class="flex items-center justify-end gap-3 mb-5">
+              <IconsLogosEmailSquare />
+              <IconsLogosGithubSquare />
+              <IconsLogosLinkedinSquare />
+            </div>
+            <CarbonBadge />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+</script>
