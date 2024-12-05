93a01b43bfb8635d102478e289fba66bde3ac23f
Ines Alvergne <i.alvergne@gmail.com>
2024-12-05 19:45:10 +0100

    clean layout

--- app.vue
+++ app.vue
@@ -1,13 +1,5 @@
 <template>
   <NuxtLayout>
-    <div class="flex flex-col min-h-screen">
-      <div class="flex-grow">
-        <Navbar />
-        <div class="w-11/12 laptop:w-9/12 desktop:w-7/12 mx-auto">
-          <NuxtPage />
-        </div>
-      </div>
-    </div>
-    <Footer />
+    <NuxtPage />
   </NuxtLayout>
 </template>
