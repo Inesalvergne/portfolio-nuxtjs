5a7e8676d29242ea8a326b905ade9c9f45a76790
Ines Alvergne <i.alvergne@gmail.com>
2024-12-16 18:18:34 +0100

    translation services page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,8 +29,8 @@ export default defineNuxtConfig({
     langDir: "locales",
     lazy: true,
     locales: [
-      { code: "en", iso: "en-US", files: ['en/nav.json', 'en/about.json'] },
-      { code: "fr", iso: "fr-FR", files: ['fr/nav.json', 'fr/about.json'] },
+      { code: "en", iso: "en-US", files: ['en/nav.json', 'en/about.json', 'en/services.json'] },
+      { code: "fr", iso: "fr-FR", files: ['fr/nav.json', 'fr/about.json', 'fr/services.json'] },
     ],
     strategy: "prefix",
   },
--- pages/services.vue
+++ pages/services.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <Header
-      title="Let's build something great together"
-      description="Whether your want to start from scratch or add new features to your project, I'm here to help.
-                  You can hire me as a solo dev or I can join your team for one or all of the services below."
+      :title= "$t('services.header.title')"
+      :description= "$t('services.header.description')"
     />
 
     <CardsService
@@ -47,6 +46,10 @@ import IconsShapesInfinity from './components/icons/shapes/Infinity.vue';
 import IconsShapesWarm from './components/icons/shapes/Warm.vue';
 import IconsShapesTeewee from './components/icons/shapes/Teewee.vue';
 
+  import { useI18n } from 'vue-i18n';
+
+  const { t } = useI18n();
+
   definePageMeta({
     title: "Services",
     description: "Looking for a developer who gets your vision? I design and build websites to help grow your business. Let's make something great together!"
@@ -56,36 +59,36 @@ import IconsShapesTeewee from './components/icons/shapes/Teewee.vue';
   {
     "bgColor": "bg-transparent-pastel-1",
     "illustration": IconsShapesArc,
-    "title": "Zero to one - Scopes & Prototypes",
-    "description": "I’ll help you understand your audience, map out user journeys, and outline the structure of your app. From there, we’ll create interactive prototypes that bring your vision to life with a focus on your brand and user experience.",
+    "title": t("services.cards.scoping.title"),
+    "description": t("services.cards.scoping.description"),
     "icons": [IconsLogosFigma]
   },
   {
     "bgColor": "bg-transparent-pastel-2",
     "illustration": IconsShapesTeewee,
-    "title": "Hit the ground running - Build a website or app",
-    "description": "Whether you're looking to build an MVP or add new features to an existing app, I specialize in building efficient, scalable solutions with Ruby on Rails. Together, we’ll ensure your app evolves alongside your business goals.",
+    "title": t("services.cards.development.title"),
+    "description": t("services.cards.development.description"),
     "icons": [IconsLogosRuby, IconsLogosRails, IconsLogosJavascript, IconsLogosStimulus, IconsLogosTailwind, IconsLogosBootstrap, IconsLogosNuxt, IconsLogosVue]
   },
   {
     "bgColor": "bg-transparent-pastel-3",
     "illustration": IconsShapesWarm,
-    "title": "Connect & Grow - APIs integrations",
-    "description": "Let’s expand your app’s capabilities by integrating APIs to enhance functionality. From payment processing to CRMs, I'll seamlessly connect your app to the services you need.",
+    "title": t("services.cards.apis.title"),
+    "description": t("services.cards.apis.description"),
     "icons":  [IconsLogosStripe, IconsLogosZoom, IconsLogosDropbox, IconsLogosGoogleCloud, IconsLogosBrevo]
   },
   {
     "bgColor": "bg-transparent-pastel-1",
     "illustration": IconsShapesDirection,
-    "title": "Ready to shine - Tests, deploy & collaboration",
-    "description": "With a focus on quality and reliability, I’ll handle testing and deployment to ensure smooth launches. I’m ready to collaborate with your team, maintain code quality, and keep your project moving forward.",
+    "title": t("services.cards.deploy.title"),
+    "description": t("services.cards.deploy.description"),
     "icons":  [IconsLogosRspec, IconsLogosHeroku, IconsLogosNetlify, IconsLogosGithub, IconsLogosGitlab]
   },
   {
     "bgColor": "bg-transparent-pastel-2",
     "illustration": IconsShapesInfinity,
-    "title": "Conquer the market - Boost & track traffic",
-    "description": "Let’s bring new users to your app and keep them engaged! Using my experience in Marketing, I’ll help you set up tools to monitor traffic and implement strategies for growth.",
+    "title": t("services.cards.boost.title"),
+    "description": t("services.cards.boost.description"),
     "icons": [IconsLogosGoogleAnalytics, IconsLogosGoogleTagManager, IconsLogosSemrush]
   }
 ]
